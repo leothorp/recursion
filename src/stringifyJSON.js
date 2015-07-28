108c352e4f0ead806b1f8eb5e7551631ee72f072
leothorp <leo.thorp1@gmail.com>
2015-07-28 12:36:07 -0400

    Streamlining stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,27 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var string;
+  //case for null
+  if (obj === null) {  
+    string = 'null';
+  //case for objects and arrays 
+  } else if (typeof obj === 'object') {      
+    var temp2 = [];
+    var isArray = Array.isArray(obj);
+    var stringEnds = isArray ? ['[',']'] : ['{','}'];
+    _.each(obj, function(element, key) {
+      if (!(typeof element ==='function' || typeof element ==='undefined')) {
+        temp2.push((isArray ? '' : stringifyJSON(key) + ':') + stringifyJSON(element));
+      }  
+    });
+    string = stringEnds[0] + temp2.join(',') + stringEnds[1];
+  //case for strings  
+  } else if (typeof obj === 'string') {         
+    string = '"' + obj + '"';
+  } else {      
+  //case for numbers and booleans 
+    string = obj + '';
+  } 
+  return string;     
+}; 
